8a4fe50e88afd83a9108f8fd75a1c3ea1e56e577
Spencer Gannon <spencer.gannon@valpo.edu>
2024-09-01 08:39:17 -0500

    Added rodio and rtmidi to package list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
+use std::fs::File;
+use std::io:BufReader;
+use rodio::{Decoder, OutputStream source::Source};	// Rust audio
+use rtmidi::{RtMidiIn, RtMidiOut, RtMidiError};		// getting MIDI input
+
+fn main() -> Result<(), RtMidiError> {
     println!("Hello, world!");
 }
