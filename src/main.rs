c6b19715e7c65b0ac4d08823be07419e77df423a
Spencer Gannon <spencer.gannon@valpo.edu>
2024-09-22 10:03:32 -0500

    Added file selection, moved into own file, currently not working, but works in main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::fs::File;
+use std::path::Path;
 use std::io::BufReader;
+use std::io::stdin;
 use rodio::{Decoder, OutputStream, Sink, source::Source};	// Rust audio
 // use rtmidi::{RtMidiIn, RtMidiOut, RtMidiError};		// getting MIDI input
 use rfd::FileDialog;	// picking files
@@ -66,22 +68,41 @@ const VERSION:&str = "v0.1.0";
 // 	// https://github.com/marc2332/freya/blob/main/examples/file_explorer.rs
 // }
 
-fn main() {
-	let files = FileDialog::new()
+fn chooseFile(user_path: &Path){
+	let path = std::env::current_dir().unwrap();
+
+	let file = FileDialog::new()
 		.add_filter("music", &["mp3", "wav"])
-		.set_directory("/")
+		.set_directory(&path)
 		.pick_file();
+
+	user_path = file.as_ref().unwrap().as_path();
+	println!("[DEBUG] The user chose: {:#?}", user_path);
+}
+
+fn main() {
+	let mut user_path = Path::new();
 	// Get an output stream handle to the default physical sound device
 	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 	let sink = Sink::try_new(&stream_handle).unwrap();
 	// Load a sound from a file, using a path relative to Cargo.toml
-	let file = BufReader::new(File::open("pacer_test.mp3").unwrap());
+	chooseFile(&user_path);
+	let file = BufReader::new(File::open(user_path).unwrap());
 	// Decode that sound file into a source
 	let source = Decoder::new(file).unwrap();
 	// Play the sound directly on the device
 	sink.append(source);
-
+	
 	// The sound plays in a separate audio thread,
 	// so we need to keep the main thread alive while it's playing.
-	sink.sleep_until_end();
+	//sink.sleep_until_end();
+
+	let mut user_input = String::new();
+	println!("Enter one of the following options into the prompt:");
+	println!("p: pause");
+	println!("n: next");
+	println!(">: play");
+	println!("s: seek");
+	println!("l: time left");
+	stdin().read_line(&user_input).expect("[ERROR] Failed to read line!");
 }
