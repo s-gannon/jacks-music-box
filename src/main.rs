c504c40de4adf786e1f29d036b9ea2ff397a13a0
Spencer <spencer.gannon@valpo.edu>
2024-11-09 08:57:04 -0600

    Rewrote choose_file to be command line, will implement in file selector later. Pause and exit work, play / resume does not work and exit does not work after pause

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::thread;
 use std::fs::File;
 use std::path::Path;
 use std::io::BufReader;
@@ -68,26 +69,32 @@ const VERSION:&str = "v0.1.0";
 // 	// https://github.com/marc2332/freya/blob/main/examples/file_explorer.rs
 // }
 
-fn chooseFile(user_path: &Path){
-	let path = std::env::current_dir().unwrap();
+fn choose_file() -> Option<String> {
+	println!("Please select an audio file.");
+	let mut filename = String::new();
+	loop {
+		print!("{}", &filename);
+		stdin().read_line(&mut filename).expect("Failed to read line");
+		if !filename.is_empty() && filename.ends_with(".wav") || filename.ends_with(".mp3") || filename.ends_with(".ogg") {
+			break;
+		}
+		println!("Please enter a valid file name.");
+		filename.clear();
+	}
 
-	let file = FileDialog::new()
-		.add_filter("music", &["mp3", "wav"])
-		.set_directory(&path)
-		.pick_file();
-
-	user_path = file.as_ref().unwrap().as_path();
-	println!("[DEBUG] The user chose: {:#?}", user_path);
+	let path = Path::new(&filename);
+	Some(path.to_str().unwrap().to_string())
 }
 
+
 fn main() {
-	let mut user_path = Path::new();
 	// Get an output stream handle to the default physical sound device
 	let (_stream, stream_handle) = OutputStream::try_default().unwrap();
 	let sink = Sink::try_new(&stream_handle).unwrap();
 	// Load a sound from a file, using a path relative to Cargo.toml
-	chooseFile(&user_path);
-	let file = BufReader::new(File::open(user_path).unwrap());
+
+	let file = BufReader::new(File::open("feather.mp3").unwrap());
+	// let file = BufReader::new(File::open(choose_file().expect("No file was chosen!")).unwrap());
 	// Decode that sound file into a source
 	let source = Decoder::new(file).unwrap();
 	// Play the sound directly on the device
@@ -98,11 +105,25 @@ fn main() {
 	//sink.sleep_until_end();
 
 	let mut user_input = String::new();
-	println!("Enter one of the following options into the prompt:");
-	println!("p: pause");
-	println!("n: next");
-	println!(">: play");
-	println!("s: seek");
-	println!("l: time left");
-	stdin().read_line(&user_input).expect("[ERROR] Failed to read line!");
+	loop {
+		println!("Enter one of the following options into the prompt:");
+		println!("p: pause");
+		println!("n: next");
+		println!(">: play");
+		println!("s: seek");
+		println!("l: time left");
+		println!("e: exit");
+		stdin().read_line(&mut user_input).expect("[ERROR] Failed to read line!");
+
+		if user_input.eq("e\n"){
+			break;
+		}
+		else if user_input.eq("p\n") {
+			sink.pause();
+		}
+		else if user_input.eq(">\n") {
+			sink.play();
+		}
+	}
+		
 }
